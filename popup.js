1a21cf0311bfc398ff0b948692d87984b0c83677
Jing Li <thyrlian@gmail.com>
2013-12-16 21:37:17 +0100

    Implemented getURLs function.

---
+++ popup.js
@@ -0,0 +1,17 @@
+function getURLs(callback) {
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+        var urls = [];
+        for (var i = 0; i < tabs.length; i++) {
+            urls.push(tabs[i].url);
+        }
+        callback(urls);
+    });
+}
+
+function saveURLs() {
+    
+}
+
+function openURLs() {
+    
+}
